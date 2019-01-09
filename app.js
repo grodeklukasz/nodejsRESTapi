f0a4cf43365824750cd4b0766702e08d41087157
grodek <Lukasz.Grodek@wjwgmbh.local>
2019-01-09 11:18:50 +0100

    added body parser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import db from './db/db';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+//Parse incomming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //get all todos
 
@@ -10,6 +15,35 @@ app.get('/api/v1/todos', (req, res)=>{
         success: 'true',
         message: 'todos retrieved success',
         todos: db
+    })
+});
+
+app.post('/api/v1/todos', (req,res)=>{
+    console.log(req.body.title);
+    if(!req.body.title){
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    }else if(!req.body.description){
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required'
+        });
+    }
+
+    const todo = {
+        id: db.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    
+    db.push(todo);
+
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        todo
     })
 });
 
